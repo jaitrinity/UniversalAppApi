85740ea859402688716e2d24f8ff1cbbc57bbd6c
Jai Trinity <jai.prakash@trinityapplab.co.in>
2024-11-07 13:13:30 +0530

    remove api key

--- distanceTest.php
+++ distanceTest.php
@@ -1,5 +1,5 @@
 <?php 
-$api_key = "AIzaSyBzin8T1YLrzvFY80YjLH86nN1LeV2EzRU";
+$api_key = "[api_key]";
 $origin = "28.6229897,77.3663686";
 $distinations = "36.9354515,44.0343305";
 $url='https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins='.$origin.'&destinations='.$distinations.'&key='.$api_key;
