5b6bee205b157712d7eab02a28c552fc307e1853
Jai Trinity <jai.prakash@trinityapplab.co.in>
2026-01-23 14:56:08 +0530

    CI/CD pipeline1

--- dbConfiguration.php
+++ dbConfiguration.php
@@ -2,12 +2,17 @@
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers:content-type");
-$conn = new mysqli(
-    getenv('DB_HOST'),
-    getenv('DB_USER'),
-    getenv('DB_PASS'),
-    getenv('DB_NAME'),
-    3306
-);
+$DB_HOST = getenv('DB_HOST');
+$DB_USER = getenv('DB_USER');
+$DB_PASS = getenv('DB_PASS');
+$DB_NAME = getenv('DB_NAME');
+$conn = mysqli_connect($DB_HOST,$DB_USER,$DB_PASS,$DB_NAME);
+// $conn = new mysqli(
+//     getenv('DB_HOST'),
+//     getenv('DB_USER'),
+//     getenv('DB_PASS'),
+//     getenv('DB_NAME'),
+//     3306
+// );
 mysqli_set_charset($conn, 'utf8');
 ?>
