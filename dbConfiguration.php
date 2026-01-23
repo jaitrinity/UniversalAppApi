0b2b7ffdf8b9fa0d3777aa6b4d1f363234eb52a9
Jai Trinity <jai.prakash@trinityapplab.co.in>
2026-01-23 15:31:19 +0530

    ci/cd pipelline1

--- dbConfiguration.php
+++ dbConfiguration.php
@@ -2,17 +2,6 @@
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers:content-type");
-$DB_HOST = getenv('DB_HOST');
-$DB_USER = getenv('DB_USER');
-$DB_PASS = getenv('DB_PASS');
-$DB_NAME = getenv('DB_NAME');
-$conn = mysqli_connect($DB_HOST,$DB_USER,$DB_PASS,$DB_NAME);
-// $conn = new mysqli(
-//     getenv('DB_HOST'),
-//     getenv('DB_USER'),
-//     getenv('DB_PASS'),
-//     getenv('DB_NAME'),
-//     3306
-// );
+$conn = mysqli_connect("localhost","UniversalApp","YQrdnH6hX3","UniversalApp");
 mysqli_set_charset($conn, 'utf8');
 ?>
